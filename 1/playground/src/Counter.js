66ea8c24e88304abc8cdc4da114513a900819351
me <a@b.c>
2019-09-05 11:25:13 +0200

    Add reset button

--- 1/playground/src/Counter.js
+++ 1/playground/src/Counter.js
@@ -5,7 +5,10 @@ function Counter() {
     // current value and setter for the value are returned from useState
     const [count, setCount] = useState(0);
 
-    return <button onClick={() => {setCount(count + 1)}}>{count}</button>;
+    return (<div>
+        <button onClick={() => {setCount(count + 1)}}>{count}</button>
+        <button onClick={() => {setCount(0)}}>Reset</button>
+    </div>);
 }
 
 export default Counter;
