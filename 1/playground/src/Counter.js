7c8f2e73adae7c3332c7156ad8f8944485e9184f
me <a@b.c>
2019-09-05 11:20:59 +0200

    Add state to counter

--- 1/playground/src/Counter.js
+++ 1/playground/src/Counter.js
@@ -1,7 +1,11 @@
-import React from "react";
+// React is default export and useState is another export
+import React, { useState } from "react";
 
 function Counter() {
-    return <button>0</button>;
+    // current value and setter for the value are returned from useState
+    const [count, setCount] = useState(0);
+
+    return <button>{count}</button>;
 }
 
 export default Counter;
