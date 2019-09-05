2a7e21821952485920241d4dec4a07349727c8a9
me <a@b.c>
2019-09-05 16:19:38 +0200

    Render stars depending on rating

--- 1/playground/src/Rating.js
+++ 1/playground/src/Rating.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './stars.css'
+
+// TODO: 
+// return Array of spans
+// set style with CSS of each span
+// Use unicode for the star
+
+const starIDs = [1, 2, 3, 4, 5]
 
 function Rating({stars}) {
-    return <> { "*".repeat(stars) } </>
-
+    
+    function getStar(id)
+    {
+        let res = id > stars ? "☆" : "★"
+        let sstyle = id > stars ? "star off": "star on"
+        return <span className={sstyle}>{res}</span>
+    }
+    
+    return <> { starIDs.map(id => getStar(id)) } </>
 }
 
 export default Rating;
