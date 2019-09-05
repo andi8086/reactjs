26f510598616ad34961746cddb8846d86e89ed3a
me <a@b.c>
2019-09-05 15:12:54 +0200

    Add style to counter

--- 1/playground/src/Counter.js
+++ 1/playground/src/Counter.js
@@ -1,11 +1,13 @@
 // React is default export and useState is another export
 import React, { useState } from "react";
+import './counter.css'
 
 function Counter() {
     // current value and setter for the value are returned from useState
     const [count, setCount] = useState(0);
 
-    return (<div>
+    return (
+    <div className={"counter " + (count > 10 ? "high": "low")}>
         <button onClick={() => {setCount(count + 1)}}>{count}</button>
         <button onClick={() => {setCount(0)}}>Reset</button>
     </div>);
