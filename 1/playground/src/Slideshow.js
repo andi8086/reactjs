60fd12a1bdb9bb2acdd11d77d5e8925389598252
me <a@b.c>
2019-09-05 13:43:26 +0200

    Do prev and next buttons

--- 1/playground/src/Slideshow.js
+++ 1/playground/src/Slideshow.js
@@ -6,23 +6,24 @@ function SlideShow() {
 
     let imgsrc = 'https://picsum.photos/200?image=' + imgnum.toString();
 
+    // use {"<"} or &lt; for <. The first is prefered
     return (<div>
         <img src={imgsrc}></img>
         <div>
-            <button onClick={() => {
-                setimgnum(imgnum + 1)
-                }}>
-                next
-            </button>
             <button onClick={() => {
                 if (imgnum > 0) {
                     setimgnum(imgnum - 1)
                 }
                 }}>
-                prev
+                {"<"}
+            </button>
+            <button onClick={() => {
+                setimgnum(imgnum + 1)
+                }}>
+                {">"}
             </button>
         </div>
-        
+
         </div>)
 
 
