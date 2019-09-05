7dc43b551e3d87389048632067e6aa404d683efe
me <a@b.c>
2019-09-05 10:58:07 +0200

    Add random text

--- 1/playground/src/App.js
+++ 1/playground/src/App.js
@@ -7,11 +7,20 @@ import './App.css';
 // < changes to XML and { changes to JS
 // dynamic parts within XML cannot contain objects
 function App() {
+
+  let coinText
+  if (Math.random() < 0.5) {
+      coinText = "Kopf"
+  } else {
+      coinText = "Zahl"
+
+  }
   return (
     <div className="App">
       <h1>Playground</h1>
       <div> Ein Jahr hat {365 * 24} Stunden</div>
       <div> Heute ist der {new Date().toLocaleDateString()}</div>
+      <div> {coinText}</div>
     </div>
   );
 }
